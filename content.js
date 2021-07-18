591f5477cf72ebef2cd04a6cac32455be3436c09
elee2001 <hoyin_3@hotmail.com>
2021-07-18 11:42:22 -0700

    Have event listeners, but can't get JSON request to work

--- content.js
+++ content.js
@@ -1,9 +1,30 @@
-alert('my extension');
-chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-    let url = tabs[0].url;
-    // use `url` here inside the callback because it's asynchronous!
+var xhr = new XMLHttpRequest();
+xhr.onload = function() {
+    var json = xhr.responseText;                         // Response
+    json = json.replace(/^[^(]*\(([\S\s]+)\);?$/, '$1'); // Turn JSONP in JSON
+    json = JSON.parse(json);                             // Parse JSON
+    // ... enjoy your parsed json...
+};
+// Example:
+// data = 'Example: appended to the query string..';
+// xhr.open('GET', 'http://domain/getjson?data=' + encodeURIComponent(data));
+// xhr.send();
 
-    if (url) {
-        alert(url);
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+      if( request.message === "Navigated" ) {
+        let curLocation = window.location.href;
+        if (curLocation.includes("dept=") && curLocation.includes("course=")) {
+          //Then there is a specific course we can get averages for, so let's parse the course name and number
+          //and send a json request to UBCGrades API
+          // alert('aaa')
+          const urlParams = new URLSearchParams(curLocation);
+          let data = urlParams.get("dept") + "/" + urlParams.get("course")
+          let JSONRequest = "https://ubcgrades.com/api/v2/grades/UBCV/2020W/" + encodeURIComponent(data);
+          xhr.open('GET', JSONRequest);
+          xhr.send();
+        }
+      }
     }
-});
+  );
+
