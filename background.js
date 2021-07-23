c12863274ce29b86e1266b7e552872cb483a7d0a
Ethan Lee <hoyin_3@hotmail.com>
2021-07-22 22:36:27 -0700

    JSON Request seems formatted correctly, but is failing with Error 505 still

--- background.js
+++ background.js
@@ -5,3 +5,23 @@ chrome.tabs.onUpdated.addListener(function(tab) {
       chrome.tabs.sendMessage(activeTab.id, {"message": "Navigated"});
     });
   });
+
+  chrome.runtime.onMessage.addListener(function(request, sender, callback) {
+    if (request.action == "xhttp") {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onload = function() {
+            callback(xhttp.responseText);
+        };
+        xhttp.onerror = function() {
+            // Do whatever you want on error. Don't forget to invoke the
+            // callback to clean up the communication port.
+            callback();
+        };
+        xhttp.open('GET', request.url);
+        // if (method == 'POST') {
+            // xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        // }
+        xhttp.send();
+        return true; // prevents the callback from being called too early on return
+    }
+});
--- content.js
+++ content.js
@@ -1,8 +1,10 @@
 var xhr = new XMLHttpRequest();
+var JSONRequest;
 xhr.onload = function() {
     var json = xhr.responseText;                         // Response
     json = json.replace(/^[^(]*\(([\S\s]+)\);?$/, '$1'); // Turn JSONP in JSON
-    json = JSON.parse(json);                             // Parse JSON
+    json = JSON.parse(json);                             // Parse JSON\
+    alert(JSON);
     // ... enjoy your parsed json...
 };
 // Example:
@@ -18,11 +20,24 @@ chrome.runtime.onMessage.addListener(
           //Then there is a specific course we can get averages for, so let's parse the course name and number
           //and send a json request to UBCGrades API
           // alert('aaa')
-          const urlParams = new URLSearchParams(curLocation);
+          let urlParams = new URLSearchParams(curLocation);
           let data = urlParams.get("dept") + "/" + urlParams.get("course")
-          let JSONRequest = "https://ubcgrades.com/api/v2/grades/UBCV/2020W/" + encodeURIComponent(data);
-          xhr.open('GET', JSONRequest);
-          xhr.send();
+          JSONRequest = "/v2/grades/UBCV/2020W/" + encodeURIComponent(data);
+          chrome.runtime.sendMessage({
+            method: 'POST',
+            action: 'xhttp',
+            url: 'https://ubcgrades.com/api' + JSONRequest
+            // , data: JSONRequest
+        }, function(responseText) {
+          // console.warn(xhr.responseText)
+            // alert('Got the response text');
+            // responseText = responseText.replace(/^[^(]*\(([\S\s]+)\);?$/, '$1'); // Turn JSONP in JSON
+            // responseText = JSON.parse(responseText);                             // Parse JSON\
+            // console.log(responseText);
+            /*Callback function to deal with the response*/
+        });
+          return true;
+
         }
       }
     }
